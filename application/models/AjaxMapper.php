842b42a55e78377c8a0c5b58ef5b906cdbd5c010
Mika Saari <mika@web.tmeri.fi>
2012-03-17 22:36:11 +0200

    Ajax side SQL queries changed

--- application/models/AjaxMapper.php
+++ application/models/AjaxMapper.php
@@ -53,17 +53,19 @@ class Application_Model_AjaxMapper
 
 		if($having == "")
 		{
-			$query = "select media_file.media_id, media_file.file_id,files.hash_name from media_file,files,(select media_id, count(media_id) as amount from (select media.id as media_id from media join media_tag on id = media_id join tags on tag_id=tags.id where tags.tag in $tags union all select media.id as media_id from media join media_file on media.id=media_file.file_id join files on files.id=file_id join file_tag on files.id=file_tag.file_id join tags on tag_id=tags.id where tags.tag in $tags) as tbl group by media_id having amount=$size) as tbl2 where tbl2.media_id=media_file.media_id and files.id=media_file.file_id";
+			$query = "select media_file.media_id, media_file.file_id,files.hash_name from media_file,files,(select media_id, count(media_id) as amount from (select media.id as media_id from media join media_tag on id = media_id join tags on tag_id=tags.id where tags.tag in $tags union all select media.id as media_id from media join media_file on media.id=media_file.media_id join files on files.id=file_id join file_tag on files.id=file_tag.file_id join tags on tag_id=tags.id where tags.tag in $tags) as tbl group by media_id having amount>=$size) as tbl2 where tbl2.media_id=media_file.media_id and files.id=media_file.file_id group by media_id";
 		}
 		else if ($tags == "" and $having != "")
 		{
-			$query = "select media_file.media_id, media_file.file_id,files.hash_name from media_file,files,(select media_id, count(media_id) as amount from (select media.id as media_id from media join media_tag on id = media_id join tags on tag_id=tags.id where tags.tag like '$having%' union all select media.id as media_id from media join media_file on media.id=media_file.file_id join files on files.id=file_id join file_tag on files.id=file_tag.file_id join tags on tag_id=tags.id where tags.tag like '$having%') as tbl group by media_id having amount=1) as tbl2 where tbl2.media_id=media_file.media_id and files.id=media_file.file_id";
+			$query = "select media_file.media_id, media_file.file_id,files.hash_name from media_file,files,(select media_id, count(media_id) as amount from (select media.id as media_id from media join media_tag on id = media_id join tags on tag_id=tags.id where tags.tag like '$having%' union all select media.id as media_id from media join media_file on media.id=media_file.file_id join files on files.id=file_id join file_tag on files.id=file_tag.file_id join tags on tag_id=tags.id where tags.tag like '$having%') as tbl group by media_id having amount=1) as tbl2 where tbl2.media_id=media_file.media_id and files.id=media_file.file_id group by media_id";
 		}
 		else
 		{
 			$query="select media_id, SUM(amount) as amount, hash_name from (select tbl1.media_id, amount, files.hash_name from (select media_id, count(media_id) as amount from media_tag join tags on tags.id=media_tag.tag_id where tags.tag in $tags group by media_id having amount=".($size-1).") as tbl1, files, media_file where tbl1.media_id=media_file.media_id and media_file.file_id=files.id union all select tbl1.media_id, amount, files.hash_name from (select media_id, count(media_id) as amount from media_tag join tags on tags.id=media_tag.tag_id where tags.tag like '$having%' group by media_id) as tbl1, files, media_file where tbl1.media_id=media_file.media_id and media_file.file_id=files.id union all select media_id, count(media_id) as amount, files.hash_name from media_file join files on media_file.file_id=files.id join file_tag on files.id=file_tag.file_id join tags on file_tag.tag_id=tags.id where tags.tag in $tags union all select media_id, count(media_id), files.hash_name as amount from media_file join files on media_file.file_id=files.id join file_tag on files.id=file_tag.file_id join tags on file_tag.tag_id=tags.id where tags.tag like '$having%') as tbl3 group by media_id having amount >=$size";
 
 		}
+
+		error_log("QUERY: $query");
 
 		$stmt = $db->query($query);
 		return $stmt->fetchAll();
